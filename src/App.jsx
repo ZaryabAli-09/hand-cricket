c0a229eb40f79beb1f617f277c7e0bb680d18177
Zaryab <zaryabkhan248@gmail.com>
2024-07-10 23:03:17 +0500

    start ui completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+import MaleAvatar from "./assets/male.png";
+import FemaleAvatar from "./assets/female.png";
+import { FaComputer } from "react-icons/fa6";
+import { FaDice } from "react-icons/fa";
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <main className="p-10 bg-tertiary-color max-w-lg mx-auto">
+      <div className="bg-secondary-color p-5 rounded-lg flex flex-col items-center">
+        <p className="text-center font-extrabold text-4xl  text-primary-color">
+          Hello Hand
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <h1 className="text-center font-extrabold text-5xl ">
+          Cricket<span className="text-primary-color">.</span>
+        </h1>
+        <input
+          className="mt-8 p-1 rounded-lg text-sm placeholder-gray-400 text-center px-6 focus:outline-none border-4  focus:border-primary-color font-bold"
+          placeholder="Name (4-6 characters)"
+          type="text"
+        />
+        <div className="flex space-x-3 mt-5 ">
+          <div className="p-2 border-4 border-primary-color rounded-md hover:bg-primary-color cursor-pointer">
+            <img className="w-[40px]" src={MaleAvatar} alt="" />
+          </div>
+          <div className="p-2 border-4 border-primary-color rounded-md hover:bg-primary-color cursor-pointer">
+            <img className="w-[40px]" src={FemaleAvatar} alt="" />
+          </div>
+        </div>
 
-export default App
+        <h3 className="mt-5 font-bold text-primary-color">
+          ------Single Player------
+        </h3>
+        <div className="flex space-x-2 mt-5">
+          <div
+            className="flex flex-col items-center border-4 border-primary-color
+          p-2 rounded-md bg-white hover:bg-primary-color cursor-pointer hover:text-white w-[90px]"
+          >
+            <FaComputer className="text-xl" />{" "}
+            <span className="text-xs font-extrabold ">Computer</span>
+          </div>{" "}
+          <div
+            className="flex flex-col items-center border-4 border-primary-color
+          p-2 rounded-md bg-white hover:bg-primary-color cursor-pointer hover:text-white w-[90px]"
+          >
+            <FaDice className="text-xl" />{" "}
+            <span className="text-xs font-extrabold ">Random</span>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
